31e0aac478f463b4331ebf68c2e15a5a8cc7a7a0
jeyjunior <jeyjuniordg@gmail.com>
2021-09-15 14:16:55 -0300

    Aula 4 - ActivityLogin

--- app/src/main/java/com/jjunior/estaohack/SplashActivity.kt
+++ app/src/main/java/com/jjunior/estaohack/SplashActivity.kt
@@ -8,25 +8,14 @@ import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
 
-    //onCreate é um dos métodos presente no ciclo de vida da Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //Método que faz referência a nossa activity splash res/layout/activity_splash
         setContentView(R.layout.activity_splash)
-
-        //Abrir a MainActivity após 5 segundos
         Handler(Looper.getMainLooper()).postDelayed({
-            //postDelayed passar como argumento uma função lambda {}
-
-            //Iniciar uma intent - transição da tela Splash para a tela Main
-            //Selecionar Intent() segura alt e aperta entre pra importar as bibliotecas
-            val mIntent = Intent(this, MainActivity::class.java)
-
-            startActivity(mIntent) //Iniciar Essa tela (as telas ficam empilhadas)
-            //Nesse ponto vamos finalizar a tela SplashScreen
+            val mIntent = Intent(this, LoginActivity::class.java)
+            startActivity(mIntent)
 
             finish()
-        }, 5000) //5000ms
+        }, 1000)
     }
 }
