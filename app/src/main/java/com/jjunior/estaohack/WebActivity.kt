a50b7bdab4f9033f1863d15b84f58d47aef25a66
jeyjunior <jeyjuniordg@gmail.com>
2021-09-21 12:12:45 -0300

    Aula 6 - WebActivity full code

--- app/src/main/java/com/jjunior/estaohack/WebActivity.kt
+++ app/src/main/java/com/jjunior/estaohack/WebActivity.kt
@@ -2,10 +2,26 @@ package com.jjunior.estaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.jjunior.estaohack.databinding.ActivityWebBinding
 
 class WebActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web)
+        binding = ActivityWebBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Habilitar execução js
+        binding.wbvWeb.settings.javaScriptEnabled = true
+
+        //load url
+        binding.wbvWeb.loadUrl("https://br.cellep.com/estacaohack")
+
+        //Definindo webview cliente padrão
+        binding.wbvWeb.webViewClient = WebViewClient()
+
     }
 }
