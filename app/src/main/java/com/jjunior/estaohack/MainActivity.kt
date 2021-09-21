28bf93920710444b85a97c0b417833c1fd67883a
jeyjunior <jeyjuniordg@gmail.com>
2021-09-21 12:08:45 -0300

    Aula 6 - MainActivity full code

--- app/src/main/java/com/jjunior/estaohack/MainActivity.kt
+++ app/src/main/java/com/jjunior/estaohack/MainActivity.kt
@@ -1,14 +1,62 @@
 package com.jjunior.estaohack
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.jjunior.estaohack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding:ActivityMainBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Recuperando o valor passado pela intent
+        val email = intent.getStringExtra("INTENT_EMAIL")
+
+        //Acessar o xml referente ao cadastro do usuario logado (identificando pelo email)
+        val sharedPrefs = getSharedPreferences("Cadastro $email", Context.MODE_PRIVATE)
+
+        val nome = sharedPrefs.getString("NOME", "")
+        val sobrenome = sharedPrefs.getString("SOBRENOME","")
+        val continente = sharedPrefs.getString("CONTINENTE","")
+
+        //exibir os dados cadastrados
+        binding.txvMainNome.text = "$nome $sobrenome"
+        binding.txvMainEmail.text = email
+        binding.txvMainContinente.text = continente
+
+        //botão sair
+        binding.btnMainSair.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Atenção")
+            alert.setMessage("Deseja sair?")
+
+            //btn a direita
+            alert.setPositiveButton("Sair"){dialog, which ->
+                val mIntent = Intent(this, LoginActivity::class.java)
+                startActivity(mIntent)
+                finish()
+            }
+
+            //btn a esquerda
+            alert.setNeutralButton("Não"){dialog, which -> }
+
+            //alert.setCancelable(false) opcional
+
+            alert.show()
+        }
+
+        //botão site
+        binding.btnMainSite.setOnClickListener {
+            val mIntent = Intent(this, WebActivity::class.java)
+            startActivity(mIntent)
+        }
     }
 
 }
--- app/src/main/java/com/jjunior/estaohack/SplashActivity.kt
+++ app/src/main/java/com/jjunior/estaohack/SplashActivity.kt
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.jjunior.estaohack.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mIntent = Intent(this, LoginActivity::class.java)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            val mIntent = Intent(this, LoginActivity::class.java)
             startActivity(mIntent)
-    //
             finish()
         }, 1000)
     }
